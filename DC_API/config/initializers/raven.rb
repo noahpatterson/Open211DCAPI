f57816a54b6ee66a9dcd3fc27adfb1b78520f8c9
noahpatterson <noah@noah-patterson.com>
2014-03-19 22:47:55 -0400

    update getsentry for this api

--- DC_API/config/initializers/raven.rb
+++ DC_API/config/initializers/raven.rb
@@ -1,5 +1,5 @@
 require 'raven'
 
 Raven.configure do |config|
-  config.dsn = ''
+  config.dsn = 'https://7499f62ec64b4652acfd1e2ae19e8779:847da8f34cfc4400a5db3c40af089620@app.getsentry.com/21223'
 end
